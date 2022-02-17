48f1a5f01d48a59d0ae300ff733747bea497cd5e
Jeetn02 <65076874+Jeetn02@users.noreply.github.com>
2022-02-17 16:19:38 +0000

    Create led.py

---
+++ led.py
@@ -0,0 +1,23 @@
+
+from grove.grove_ws2813_rgb_led_strip import GroveWS2813RgbStrip
+from rpi_ws281x import Color
+import time
+
+PIN, COUNT = 12,30
+strip = GroveWS2813RgbStrip(PIN, COUNT)
+
+
+def colorWipe(strip, color, wait_ms=140):
+    """Wipe color across display a pixel at a time."""
+    for i in range(0, strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+# uv is ... ulra-violet
+color = Color(0,0,255)
+
+while True:
+    colorWipe(strip, color)
+    colorWipe(strip, Color(0, 0, 0))
+
