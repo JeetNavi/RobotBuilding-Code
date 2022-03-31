861e4d353df3f228348749f12aabc6a991df4439
Jeetn02 <65076874+Jeetn02@users.noreply.github.com>
2022-03-31 15:56:38 +0100

    Update WaterLevelSensor.py

--- WaterLevelSensor.py
+++ WaterLevelSensor.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+#CHANGE 24 TO WHICHEVER GPIO PIN YOU'RE USING
+GPIO.setup(24, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(18, 1)
+        GPIO.output(24, 1)
         sleep(0.5)
-        if GPIO.input(18):
+        if GPIO.input(24):
             print "1"
         else:
             print "0"
